f1e137f8dabbd5345472649829376d5c33b20a2b
nusratjahan7 <nusratjahan77222@gmail.com>
2026-09-20 11:50:03 +0600

    add category section for home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
 import Banner from "@/component/homepage/Banner";
 import Marquee from "@/component/homepage/Marquee";
+import CategoryShowcase, { CategoryShowcaseSkeleton } from "@/component/homepage/CategoryShowcase";
 
 export default function Home() {
   return (
     <main>
       <Banner />
       <Marquee />
+      <Suspense fallback={<CategoryShowcaseSkeleton />}>
+        <CategoryShowcase />
+      </Suspense>
     </main>
   );
 }
